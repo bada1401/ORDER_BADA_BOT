6a323a696d9dabe7aee06453a15d2c93f5ef81c7
bada1401 <akbarsho9898@gmail.com>
2025-11-19 16:26:16 +0300

    server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+{
+  "name": "badajr-bot",
+  "version": "1.0.0",
+  "description": "Telegram bot for BADA JR",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "axios": "^1.6.0",
+    "express": "^4.18.2",
+    "node-telegram-bot-api": "^0.61.0"
+  }
+}
