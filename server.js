3fb20ddfcade46c30057f4f57153b57fb46f0bdb
bada1401 <akbarsho9898@gmail.com>
2025-11-22 03:06:36 +0300

    server.js

--- server.js
+++ server.js
@@ -1,8 +1,1 @@
-import express from "express";
-import "./index.js"; // запускаем бота автоматически
 
-const app = express();
-app.get("/", (req, res) => res.send("BADA JR BOT IS RUNNING!"));
-
-const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => console.log("Server started on port", PORT));
